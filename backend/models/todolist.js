556310ab66bd93ee3d6fdf4a07ddfd7d5ef446ce
jaewan <stpjw777@gmail.com>
2021-01-29 16:19:41 +0900

    create react app

--- backend/models/todolist.js
+++ backend/models/todolist.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  contents: { type: String },
 });
 
 const Todo = mongoose.model("todo", TodoSchema);
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Test from "./components/Test";
+import Counter from "./components/counter";
+const App = () => {
+  return (
+    <>
+      <div>Hello, React</div>
+      <Test />
+      <Counter />
+    </>
+  );
+};
+
+export default App;
